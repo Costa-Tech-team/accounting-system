f40ae3242eaeabd611b3500b21a5f3a5f6864e6d
Carmelo908 <carmelopepigauna@gmail.com>
2026-07-19 09:54:43 -0300

    Added Subcategory struct to classify accounts and added funcionality to distinguish debtor and creditor accounts

--- src/account.cpp
+++ src/account.cpp
@@ -5,27 +5,45 @@
 #include <sys/types.h>
 
 Account::Account(Category category, Currentability currentability,
-                 uint8_t accountCode, uint8_t subcategoryCode,
+                 uint8_t accountCode, const Subcategory *subcategory,
                  uint8_t subaccountCode)
-  : accountCategory{category},
+  : category{category},
     currentability{currentability},
-    subcategoryCode{subcategoryCode},
+    subcategory{subcategory},
     accountCode{accountCode},
     subaccountCode{subaccountCode}
 {}
 
+Account::Nature Account::getNature(Account::Category category)
+{
+    switch (category)
+    {
+    case Account::Category::asset:
+    case Account::Category::expenseOrCost:
+        return Account::Nature::debtor;
+        break;
+    case Account::Category::liability:
+    case Account::Category::netWorth:
+    case Account::Category::revenue:
+        return Account::Nature::creditor;
+        break;
+    }
+}
+
+Account::Nature Account::getNature() { return Account::getNature(category); }
+
 std::string Account::getCode() const
 {
-    uint8_t categoryNumber = static_cast<uint8_t>(accountCategory) + 1;
+    uint8_t categoryNumber = subCode(category);
     std::string code{std::to_string(categoryNumber) + "."};
     if (currentability != Currentability::none)
     {
-        uint8_t currentabilityNumber = static_cast<uint8_t>(currentability);
+        uint8_t currentabilityNumber = subCode(currentability);
         code += std::to_string(currentabilityNumber) + ".";
     }
-    if (subcategoryCode != 0)
+    if (subcategory != nullptr)
     {
-        code += std::to_string(subcategoryCode) + ".";
+        code += std::to_string(subcategory->code) + ".";
     }
     code += std::to_string(accountCode) + ".";
     if (subaccountCode != 0)
@@ -35,15 +53,25 @@ std::string Account::getCode() const
     return code;
 }
 
-Account::Category Account::getCategory() const { return accountCategory; }
+Account::Category Account::getCategory() const { return category; }
 
 Account::Currentability Account::getCurrentability() const
 {
     return currentability;
 }
 
-uint8_t Account::getSubcategory() const { return subcategoryCode; }
+const Subcategory *Account::getSubcategory() const { return subcategory; }
 
 uint8_t Account::getAccountCode() const { return accountCode; }
 
 uint8_t Account::getSubaccountCode() const { return subaccountCode; }
+
+uint8_t subCode(Account::Category category)
+{
+    return static_cast<uint8_t>(category);
+}
+
+uint8_t subCode(Account::Currentability currentability)
+{
+    return static_cast<uint8_t>(currentability);
+}
--- src/account.hpp
+++ src/account.hpp
@@ -1,5 +1,14 @@
 #include <cstdint>
 #include <string>
+#include <string_view>
+
+/// @brief represents a subcategory of accounts so that the user can group
+/// accounts with the same category and currentability.
+struct Subcategory
+{
+    uint8_t code;
+    std::string displayName;
+};
 
 /// @brief This class contains information about an accountable account.
 ///
@@ -9,16 +18,14 @@
 /// assets in banks or cash and assets in documents) and a
 /// subaccount after the account code for specification of accounts (for
 /// example, when the user wants to note differents banks in their
-/// operations).
+/// operations). Every account has also a
 class Account
 {
   public:
-    /// This enum has the categories of accountable accounts, the last
-    /// two items are provided for convenience when checking if a account has
-    /// one of two mutually exclusives flags.
+    /// This enum has the categories of accountable accounts.
     enum class Category : uint8_t
     {
-        asset,
+        asset = 1,
         liability,
         revenue,
         expenseOrCost,
@@ -35,13 +42,27 @@ class Account
         fixed
     };
 
+    /// This enum contains the two possible natures of accounts based on it's
+    /// category.
+    enum class Nature
+    {
+        debtor,
+        creditor
+    };
+
     /// If the category is an asset or a liability, currentability must not
     /// equal none, and if the category is net worth, revenue or expense or
-    /// cost, the currentability must equal none. The optional subcategory and
-    /// subaccount are defaulted to zero.
+    /// cost, the currentability must equal none. The optional subcategory
+    /// and subaccount are defaulted to zero.
     Account(Category category, Currentability currentability,
-            uint8_t accountCode, uint8_t subcategoryCode = 0,
+            uint8_t accountCode, const Subcategory *subcategory = nullptr,
             uint8_t subaccountCode = 0);
+
+    /// @return the nature of any account given its category.
+    static Nature getNature(Category category);
+
+    /// \overload
+    Nature getNature();
 
     /// @return a code with the numerical representation of every quality of the
     /// account separed by dots. The context of the code is provided by the
@@ -50,14 +71,23 @@ class Account
 
     Category getCategory() const;
     Currentability getCurrentability() const;
-    uint8_t getSubcategory() const;
+    /// @return nullptr if not subcategory, a Subcategory instance otherwise.
+    const Subcategory *getSubcategory() const;
     uint8_t getAccountCode() const;
+    /// @return 0 if not subaccount, a code above 0 if there's subaccount
     uint8_t getSubaccountCode() const;
 
   private:
-    Category accountCategory;
+    bool postable;
+    std::string_view displayName;
+
+    Category category;
     Currentability currentability;
-    uint8_t subcategoryCode;
+    const Subcategory *subcategory;
     uint8_t accountCode;
     uint8_t subaccountCode;
 };
+
+uint8_t subCode(Account::Category category);
+
+uint8_t subCode(Account::Currentability currentability);
